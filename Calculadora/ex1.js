74789dad63444f43fb457b2ab8b05bb16b3eba27
Jeferson <jeeh71ferreira@gmail.com>
2022-05-10 13:53:52 -0300

    updating

---
+++ Calculadora/ex1.js
@@ -0,0 +1,23 @@
+function insert(num){
+    var numero = document.getElementById('resul').innerHTML
+    document.getElementById('resul').innerHTML = numero + num
+}
+
+function clean(){
+    document.getElementById('resul').innerHTML = ""
+}
+
+function back(){
+    var resultado = document.getElementById('resul').innerHTML
+    document.getElementById("resul").innerHTML = resultado.substring(0, resultado.length -1)
+}
+
+function soma(){
+    var resultado = document.getElementById('resul').innerHTML
+    if(resultado){
+        document.getElementById("resul").innerHTML = eval(resultado)
+    }
+    else{
+        document.getElementById("resul").innerHTML = "Null"
+    }
+}
--- Find-by-cep/api.py
+++
@@ -1,8 +0,0 @@
-from pesquisa import Pesquisa
-
-print("\nDigite apenas os números!")
-cep = input("Digite um CEP: ")
-
-p1 = Pesquisa(cep)
-p1.pesquisar()
-
